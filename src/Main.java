e176ebdb25b72c3d4270d8a8cf02d36e072d13ef
mesut0606 <96373369+mesut0606@users.noreply.github.com>
2022-12-06 22:20:30 +0300

    ortalama hesaplama patika dev

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       int mat, fizik, kimya, türkce, tarih, müzik;
+
+       // Scanner sınıfını ekledik
+        Scanner input= new Scanner(System.in);
+
+        // kullanıcıdan deger alıyoruz
+        System.out.print("Matematik notunu giriniz :");
+        mat = input.nextInt();
+
+        System.out.print("Fizik notunu giriniz :");
+        fizik = input.nextInt();
+
+        System.out.print("Kimya notunu giriniz :");
+        kimya = input.nextInt();
+
+        System.out.print("Türkçe notunu giriniz :");
+        türkce = input.nextInt();
+
+        System.out.print("Tarih notunu giriniz :");
+        tarih = input.nextInt();
+
+        System.out.print("Müzik notunu giriniz :");
+        müzik = input.nextInt();
+
+        double toplam = (mat + fizik + kimya + türkce + tarih + müzik);
+        double sonuc = toplam/6;
+
+        System.out.println("Ortalamanız :" + sonuc);
+
+        String s = sonuc > 60 ? "Sınıfı Geçti" : "Sınıfta Kaldı";
+        System.out.println(s);
+        }
+    }
